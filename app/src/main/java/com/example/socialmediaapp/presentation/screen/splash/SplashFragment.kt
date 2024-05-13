d0ae4f07fb6b58c015f55ad9048821d78970c4a2
Sandro-Gabunia <gabuniasandro99@gmail.com>
2024-05-13 18:38:59 +0400

    icon changed and splash screen added

---
+++ app/src/main/java/com/example/socialmediaapp/presentation/screen/splash/SplashFragment.kt
@@ -0,0 +1,19 @@
+package com.example.socialmediaapp.presentation.screen.splash
+
+import com.example.socialmediaapp.databinding.FragmentSplashBinding
+import com.example.socialmediaapp.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+    
+    override fun bind() {
+    }
+
+    override fun bindListeners() {
+    }
+
+    override fun bindObservers() {
+    }
+
+}
