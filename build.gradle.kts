2aa780a013009f696d262df59ef52352b8979f42
Sandro-Gabunia <gabuniasandro99@gmail.com>
2024-05-15 21:42:07 +0400

    google service is added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
     alias(libs.plugins.google.dagger.hilt) apply false
+    alias(libs.plugins.google.services) apply false
 //    alias(libs.plugins.google.devtools.ksp) apply false
-//    alias(libs.plugins.google.services) apply false
 }
